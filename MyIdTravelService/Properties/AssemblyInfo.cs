1d7a9f8089c42858f93e91b7167efd8132df5d91
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2020-03-11 15:46:10 -0300

    Nuget

--- MyIdTravelService/Properties/AssemblyInfo.cs
+++ MyIdTravelService/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MyIdTravelService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MyIdTravel Upload Profile Staff")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AFONSOFT")]
 [assembly: AssemblyProduct("MyIdTravelService")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("AFONSOFT ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2020.3.11")]
+[assembly: AssemblyFileVersion("2020.3.11")]
+[assembly: NeutralResourcesLanguage("pt-BR")]
