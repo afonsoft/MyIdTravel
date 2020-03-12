4af40b2d2da8f4fb383874b4c940f1ab3c73cc19
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2020-03-12 13:14:59 -0300

    Fix Version Nuget

--- MyIdTravelService/Properties/AssemblyInfo.cs
+++ MyIdTravelService/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.3.11")]
-[assembly: AssemblyFileVersion("2020.3.11")]
+[assembly: AssemblyVersion("2020.3.12")]
+[assembly: AssemblyFileVersion("2020.3.12")]
 [assembly: NeutralResourcesLanguage("pt-BR")]
